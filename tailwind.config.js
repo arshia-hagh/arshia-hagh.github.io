72df940d12eb98af7825bbd5ce921b03cf2ff1be
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-02 08:04:27 -0700

    first commit (We have no changes )

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'primery--color': '#1d1712',
+        'secondery--color': '#c86305'
+      }
+    },
+  },
+  plugins: [],
+}
+
