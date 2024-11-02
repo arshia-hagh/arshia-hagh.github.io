53a11794a8e87fa36f2052bec35b436fd6f3055d
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-02 09:47:22 -0700

    change value  primery--color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
       backgroundColor:{
-        'primery--color': '#1d1712',
+        'primery--color': '#211f1d',
         'secondery--color': '#c86305'
       }
     },
