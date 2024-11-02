03b39212e8acd64dc8611767dd8f33ac8494c070
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-02 09:46:22 -0700

    add  components layout  in file for clear code and add code discussions routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from './Components/layout/Layout'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Layout>
   )
 }
 
