d71b6422caea9199d6c2a18aaded155766eedcb5
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-04 14:21:54 -0800

    add code getData movie and showing it in ui

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import MovieItem from "../../Components/movieItem/MovieItem";
+import Container from "../../Components/container/Container";
+import { getMovie } from "../../services/axios/requsets/Movie";
+import { reqApi } from "../../services/axios/configs/Configs";
 
 function Home() {
+  const [movies, setMovies] = useState();
+  const abortController = new AbortController()
+  useEffect(() => {
+    reqApi({
+      signal: abortController.signal
+    })
+    getMovie().then(res => {
+      setMovies(res)
+    }).catch(err => console.log('Error', err.message))
+    return () => {
+      abortController.abort()
+    }
+  },[setMovies])
+  console.log(movies)
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Movies</h1>
+      <br />
+
+      <Container>
+        <div className="grid grid-cols-3">
+          <MovieItem />
+          <MovieItem />
+          <MovieItem />
+          <MovieItem />
+          <MovieItem />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
