68b6ec1dee795b79b695af85c02b5eae169a602f
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-04 14:15:36 -0800

    add  movieCart

---
+++ src/Components/movieItem/MovieItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from '../container/Container'
+
+function MovieItem() {
+  return (
+   
+
+    <div className='h-[70%] bg-primery--color w-[50%]'>
+        <div>
+            <img src="https://www.google.com/search?sca_esv=5b4135254b5cefab&q=img&udm=2&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J5MIFhvnvU242yFxzEEp3BcbXWGQjBp6XyyqfUu6Wz8hWk3bMeDVtqu_mrB1o-K4YflJcFQPwF_8HcO4g0e23yGFHocWKhGHUVyRw0szLJnh8A9Sf22qKDz0aW7pjYIFVMMH0Lk&sa=X&ved=2ahUKEwjczKj4wMOJAxUH87sIHfOZEjoQtKgLegQIHBAB&biw=1411&bih=809&dpr=1#vhid=63NFx9ztAelysM&vssid=mosaic" alt="" />
+            <h1>adsasd</h1>
+        </div>
+        <div>
+            <span>asdasda</span>
+            <span>asdasdasd</span>
+        </div>
+    </div>
+  
+  )
+}
+
+export default MovieItem
