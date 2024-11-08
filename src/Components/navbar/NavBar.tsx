25f0408b4e0e656ceac9b87b03d96b6c3ee4607f
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-08 14:11:49 -0800

    add components  searchbar in code

--- src/Components/navbar/NavBar.tsx
+++ src/Components/navbar/NavBar.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import { FaCartShopping } from "react-icons/fa6";
 import 'animate.css';
 import Button from '../button/Button';
+import Searchbar from '../searchbar/Searchbar';
 function NavBar() {
-    const [query,setQuery] = useState<string>('')
+
   return (
     <header className='bg-primery--color  w-screen flex items-center p-5 justify-between' >
         <div className='flex gap-3 animate__animated   animate__fadeInUp items-center'>
@@ -12,9 +13,8 @@ function NavBar() {
             </Button>
             <FaCartShopping className='cursor-pointer transition-transform duration-[0.3s] hover:translate-y-[-3px] ' color='white' fontSize={30}/>
         </div>
-        <form action="">
-            <input type="text" className='outline-none  px-2 py-1 border-2 rounded-2xl border-[#c86305] bg-transparent text-white' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search...' name="" id="" />
-        </form>
+        <Searchbar/>
+ 
     </header>
   )
 }
