78ec2369cfcbbc7e7e2a9450146e075c686173b3
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-09 05:31:36 -0800

    change attribute validation to validationSchema for use yup

--- src/Components/searchbar/Searchbar.tsx
+++ src/Components/searchbar/Searchbar.tsx
@@ -1,12 +1,13 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useNavigate } from "react-router-dom";
+import searchSchema from "../../Validations/Search";
 
 function Searchbar() {
   const navigate = useNavigate();
   return (
     <Formik
       onSubmit={(values) => {
-        if(values.search !== ''){
+        if(values.search !== '' && values.search.length <= 20){
             navigate(`/search?q=${values.search}`);
         }
         
@@ -14,15 +15,10 @@ function Searchbar() {
       initialValues={{
         search: "",
       }}
-      validate={(values) => {
-        const errors = {search: ''}
-        if(values.search === ''){
-            errors.search = 'Error Searchbar Empty' 
-        }
-      }}
+      validationSchema={searchSchema}
     >
       {() => (
-        <Form action="">
+        <Form action="" className="flex flex-col items-center">
           <Field
             placeholder='Search'
             className="outline-none  px-2 py-1 border-2 rounded-2xl border-[#c86305] bg-transparent text-white"
@@ -30,7 +26,7 @@ function Searchbar() {
             type="text"
           />
           <ErrorMessage name="search" >
-            {(error) => <span className="text-[#fe6a04] text-2xl">{error}</span>}
+            {(error) => <span className="text-[#fe6a04] text-sm">{error}</span>}
             
           </ErrorMessage>
         </Form>
