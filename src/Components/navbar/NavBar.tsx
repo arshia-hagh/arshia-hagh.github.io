37a0c9163621dfc18f1db33171db7433c3785ec2
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-15 04:09:50 -0800

    add condition for button login

--- src/Components/navbar/NavBar.tsx
+++ src/Components/navbar/NavBar.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+
 import { FaCartShopping } from "react-icons/fa6";
 import 'animate.css';
 import Button from '../button/Button';
 import Searchbar from '../searchbar/Searchbar';
+import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { RootState } from "../../Redux/store";
 function NavBar() {
-
+  const {login} =  useSelector((state: RootState) => state.Login)
   return (
     <header className='bg-primery--color  w-screen flex items-center p-5 justify-between' >
         <div className='flex gap-3 animate__animated   animate__fadeInUp items-center'>
+          <Link to='/Auth'>
             <Button  className='px-5  transition-transform duration-[0.3s] hover:translate-y-[-3px] py-2'>
-                LogIn
+                {login ? 'Logout' : 'Login'}
             </Button>
+          </Link>
             <FaCartShopping className='cursor-pointer transition-transform duration-[0.3s] hover:translate-y-[-3px] ' color='white' fontSize={30}/>
         </div>
         <Searchbar/>
