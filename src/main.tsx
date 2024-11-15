7e73169f62ceca76ae4f61cd0bba585d15dc66bc
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-15 04:12:41 -0800

    add provider redux in code for use redux throughout the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.ts";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
