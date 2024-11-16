2d2cf9ee5bee0a7505995fd06b9d5c19dd5b581c
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-16 08:41:34 -0800

    set onclick for button login

--- src/Components/navbar/NavBar.tsx
+++ src/Components/navbar/NavBar.tsx
@@ -6,14 +6,16 @@ import Searchbar from '../searchbar/Searchbar';
 import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/store";
+import { useLoginContext } from "../../Context/LoginContext";
 function NavBar() {
-  const {login} =  useSelector((state: RootState) => state.Login)
+  const {login,handleLogout} =  useLoginContext()
+  console.log(login)
   return (
     <header className='bg-primery--color  w-screen flex items-center p-5 justify-between' >
         <div className='flex gap-3 animate__animated   animate__fadeInUp items-center'>
           <Link to='/Auth'>
-            <Button  className='px-5  transition-transform duration-[0.3s] hover:translate-y-[-3px] py-2'>
-                {login ? 'Logout' : 'Login'}
+            <Button onClick={() => {login ? handleLogout() : ''}}  className='px-5  transition-transform duration-[0.3s] hover:translate-y-[-3px] py-2'>
+                {login  ? 'Logout' : 'Login'}
             </Button>
           </Link>
             <FaCartShopping className='cursor-pointer transition-transform duration-[0.3s] hover:translate-y-[-3px] ' color='white' fontSize={30}/>
