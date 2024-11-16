ebe20c6d480dd19e795918664184ea5a0071906e
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-16 09:00:54 -0800

    add  function in folder utils and use throughout the project

---
+++ src/utils/appUtils.ts
@@ -0,0 +1,9 @@
+import _ from "lodash"
+export const returnPaginationRagne = (totalPage:number,page:number,siblings:number) => {
+    let totalPageNoInArray = 7 + siblings
+    if(totalPageNoInArray >= totalPage){
+        return  _.range(((totalPageNoInArray + 5) - 12), totalPageNoInArray + 5)
+    }
+    
+
+}
