d8cf5c5c788e4ebbb1ff1e7b2aad6a306f5b076c
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-17 14:00:45 -0800

    add parametr  for requset api

--- src/services/axios/requsets/Movie.ts
+++ src/services/axios/requsets/Movie.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import { reqApi } from "../configs/Configs";
 
 export async function getMovie(page: number) {
-    const {data} = await reqApi.get(`/discover/movie?sort_by=popularity.desc&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
+    const {data} = await reqApi.get(`/discover/movie?sort_by=popularity.desc&language=&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
     return data
 }
