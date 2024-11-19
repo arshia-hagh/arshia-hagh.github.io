45baee9aca156a82d9b4143ea03c75c215006f28
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-19 07:05:56 -0800

    add argument for language

--- src/services/axios/requsets/Movie.ts
+++ src/services/axios/requsets/Movie.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { reqApi } from "../configs/Configs";
 
-export async function getMovie(page: number) {
-    const {data} = await reqApi.get(`/discover/movie?sort_by=popularity.desc&language=&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
+export async function getMovie(page: number,lang: string) {
+    const {data} = await reqApi.get(`/discover/movie?sort_by=popularity.desc&language=${lang}&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
     return data
 }
