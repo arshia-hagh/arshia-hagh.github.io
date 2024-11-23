5c60509e7ef40a85c135351659a12f8a5246ecdc
arshia-hr <arshiahvgrdi@gmail.com>
2024-11-23 05:17:40 -0800

    add another params

--- src/services/axios/requsets/Movie.ts
+++ src/services/axios/requsets/Movie.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { reqApi } from "../configs/Configs";
+import { getMovies } from "../../../server/types";
 
-export async function getMovie(page: number,lang: string) {
-    const {data} = await reqApi.get(`/discover/movie?sort_by=popularity.desc&language=${lang}&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
+export async function getMovie(page: number,lang: string,sortby: string) {
+    const {data} = await reqApi.get<getMovies>(`/discover/movie?sort_by=${sortby}}&language=${lang}&api_key=a0814a81d9e0ea8e164320078c18b3cb&page=${page}`)
     return data
 }
